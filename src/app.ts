e505579565cf4a8ba71c8e8149531370706410ae
Bruno Batista <brunobatysta.fla@gmail.com>
2023-02-07 09:38:43 -0300

    Instalando dependências angular

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and core components
 import * as angular from 'angular'
 import '@uirouter/angularjs'
+import './polyfills.ts';
 
 // Route
 import RouteConfig  from './route'
